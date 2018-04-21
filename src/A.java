c83608b0fa3f365c4d0bf0c56d032ec1d7bc1f6c
Maksin Shilvian <maksonblr90@gmail.com>
2018-04-21 13:21:05 +0300

    Second commit

--- src/A.java
+++ src/A.java
@@ -1,6 +1,7 @@
 public class A{
 	public static void main(String[] args){
-		Sustem.out.println("Hello World!");
+		System.out.println("Hello World!");
+		System.out.println("Hello User!");
 	}
 	
 }	
